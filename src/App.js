fa93f0a7b6d3d606882dd774b973eb593ce5e990
YouSH42 <enddl402@gmail.com>
2024-02-26 21:41:59 +0900

    second commit

--- src/App.js
+++ src/App.js
@@ -4,67 +4,44 @@ import { useState } from 'react';
 
 function App() {
 
-  let [subTitle, subTitleSet] = useState(['여자 코트 추천', '남자 코트 추천', '아동 코트 추천']);
-  let [like, likeSet] = useState(0);
-  let [modal, setModal] = useState(false);
+    let [subTitle, subTitleSet] = useState(['여자 코트 추천', '칠곡 맛집 추천', '여행 코스 추천']);
+    let [like, setLike] = useState([0, 0, 0]);
+    let [modal, setModal] = useState(false);
+    let [title, setTitle] = useState(0);
 
-  return (
-    <div className="App">
-
-      <div className="black-nav">
-        <h4>ReactBlog</h4>
-      </div>
-
-      <button onClick={() => {
-        let copy = [...subTitle];
-        copy[0] = '파이썬';
-        subTitleSet(copy);
-      }}>변경</button>
-
-      <button onClick={() => {
-        let sortArr = [...subTitle];
-        sortArr = sortArr.sort();
-        subTitleSet(sortArr)
-      }}>가나다 순 정렬</button>
-
-      <div className='list'>
-        <h4>{subTitle[0]} <span onClick={() => { likeSet(like + 1) }}>👍</span>{like}</h4>
-        <p>2월 17일 발행</p>
-      </div>
-
-      <div className='list'>
-        <h4 onClick={() => {
-          if(modal == false){
-            setModal(true)
-          }
-          else{
-            setModal(false)
-          }
-        }}> {subTitle[1]} </h4>
-        <p>2월 20일 발행</p>
-      </div>
-
-      <div className='list'>
-        <h4>{subTitle[2]}</h4>
-        <p>2월 24일 발행</p>
-      </div>
-
-      {
-        modal == true ? <Modal /> : null
-      }
-
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className="black-nav">
+                <h4>ReactBlog</h4>
+            </div>
+            {subTitle.map(function (name, i) {
+                return (
+                    <div className='list' key={i}>
+                        <h4 onClick={() => { setModal(true); setTitle(i); }}>{subTitle[i]}</h4>
+                        <span onClick={() => {
+                            let newLike = [...like];
+                            newLike[i] += 1;
+                            setLike(newLike)
+                        }}>👍</span>{like[i]}
+                        <p>2월 24일 발행</p>
+                    </div>
+                )
+            })}
+            {
+                modal == true ? <Modal subTitle={subTitle} title={title} /> : null
+            }
+        </div>
+    );
 }
 
-function Modal() {
-  return (
-    <div className='modal'>
-      <h4>제목</h4>
-      <p>날짜</p>
-      <p>상세내용</p>
-    </div>
-  )
+function Modal(props) {
+    return (
+        <div className='modal'>
+            <h4>{props.subTitle[props.title]}</h4>
+            <p>날짜</p>
+            <p>상세내용</p>
+        </div>
+    )
 }
 
 export default App;
