6150b02a2ccf68e009a41745c45cfc596ec8dabf
YouSH42 <enddl402@gmail.com>
2024-02-27 18:24:55 +0900

    third

--- src/App.js
+++ src/App.js
@@ -4,10 +4,11 @@ import { useState } from 'react';
 
 function App() {
 
-    let [subTitle, subTitleSet] = useState(['여자 코트 추천', '칠곡 맛집 추천', '여행 코스 추천']);
+    let [subTitle, setSubtitle] = useState(['여자 코트 추천', '칠곡 맛집 추천', '여행 코스 추천']);
     let [like, setLike] = useState([0, 0, 0]);
     let [modal, setModal] = useState(false);
     let [title, setTitle] = useState(0);
+    let [inputValue, setInputValue] = useState('');
 
     return (
         <div className="App">
@@ -17,16 +18,30 @@ function App() {
             {subTitle.map(function (name, i) {
                 return (
                     <div className='list' key={i}>
-                        <h4 onClick={() => { setModal(true); setTitle(i); }}>{subTitle[i]}</h4>
-                        <span onClick={() => {
-                            let newLike = [...like];
-                            newLike[i] += 1;
-                            setLike(newLike)
-                        }}>👍</span>{like[i]}
+                        <h4 onClick={() => { setModal(true); setTitle(i); }}>{subTitle[i]}
+                            <span onClick={(e) => {
+                                e.stopPropagation();
+                                let newLike = [...like];
+                                newLike[i] += 1;
+                                setLike(newLike)
+                            }}>👍</span>{like[i]}</h4>
                         <p>2월 24일 발행</p>
+                        <button onClick={()=>{
+                            let tmp = subTitle.splice(i)
+                            setSubtitle(tmp)
+                        }}>삭제</button>
                     </div>
                 )
             })}
+            <input onChange={(e) => {
+                setInputValue(e.target.value);
+                console.log(e.target.value);
+            }} />
+            <button onClick={(e) => {
+                setSubtitle(subTitle.concat(inputValue));
+                setLike(like.concat(0));
+            }}>등록</button>
+
             {
                 modal == true ? <Modal subTitle={subTitle} title={title} /> : null
             }
